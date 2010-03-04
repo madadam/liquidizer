be34e67b2046f451999959ec423644e52f0f48aa
Adam Cigánek <adam.ciganek@gmail.com>
2010-03-04 16:16:10 +0100

    Support.constant_defined? now plays nicely with Rails's automatic dependency loading

--- lib/liquidizer/support.rb
+++ lib/liquidizer/support.rb
@@ -2,16 +2,9 @@ module Liquidizer
   module Support
     # This is like Object.const_defined?, but works with namespaced constants (Foo::Bar::Baz).
     def self.constant_defined?(name)
-      base = Object
-      name.split('::').each do |name|
-        if base.const_defined?(name)
-          base = base.const_get(name)
-        else
-          return false
-        end
-      end
-
-      true
+      name.constantize && true
+    rescue NameError
+      false
     end
   end
 end
