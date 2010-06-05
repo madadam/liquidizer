7703d72cc5ca0de88a3ca346621c6dd92ecc6ce4
Oliver Clarke <clarketus@gmail.com>
2010-06-05 17:57:30 +1200

    added ability to remove liquify from controller, and check liquify status

--- lib/liquidizer/controller_extensions.rb
+++ lib/liquidizer/controller_extensions.rb
@@ -17,6 +17,9 @@ module Liquidizer
     end
 
     def render_with_liquid(options = nil, &block)
+      # use normal render if "liquify" has not been called in the controller
+      return render_without_liquid(options, &block) if !self.class.liquify_enabled?
+            
       if view_template = liquid_template_for_view(options)
         options ||= {}
         assigns = assigns_for_liquify
@@ -206,6 +209,15 @@ module Liquidizer
       def liquify(options = {})
         self.liquidizer_options = options.reverse_merge(:actions => true, :layout => true)
       end
+      
+      def remove_liquify
+        self.liquidizer_options.clear
+      end
+      
+      def liquify_enabled?
+        self.liquidizer_options != {}
+      end
+      
     end
   end
 end
